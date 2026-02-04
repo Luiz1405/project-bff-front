96eee69579d97dbcf659862e33bb6f27457dcb17
Luiz1405 <kubaszewski001@hotmail.com>
2026-02-04 19:21:47 -0300

    Adding apollo client configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Router } from './router'
 import { GlobalStyles } from './components/GlobalStyles/index.tsx'
+import { InMemoryCache, ApolloClient } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client/react'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache(),
+} as any)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalStyles />
-    <Router />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Router />
+    </ApolloProvider>
   </StrictMode>,
 )
